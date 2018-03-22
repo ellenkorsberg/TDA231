118d70671334ab329751751b45c8972dc2c17883
Lisa <sjlisa@student.chalmers.se>
2018-03-22 14:31:23 +0100

    finally correct

--- HW0/hw0_1.py
+++ HW0/hw0_1.py
@@ -10,12 +10,12 @@ cov = [[0.1, -0.05], [-0.05, 0.2]]
 def f(x,r):
     inverse = np.linalg.inv(cov)
     diff = list(map(operator.sub, x, mean))
-    return (np.transpose(diff).dot(inverse).dot(diff))/2 - r
+    return (np.transpose(diff).dot(inverse.dot(diff)))/2 - r
     #return ((np.array(map(operator.sub,x,mean)).T.tolist()*np.linalg.inv(cov)*map(operator.sub,x,mean))/2) - r
 
-x_ = np.linspace(-5, -4.5, num=200)
-y_ = np.linspace(-5, -4.5, num=200)
-x,y = np.meshgrid(x_, y_)
+x = np.linspace(-10, 10, num=200)
+y = np.linspace(-10, 10, num=200)
+xx,yy = np.meshgrid(x, y)
 levels = []
 
 for r in range(1,4):
@@ -23,24 +23,38 @@ for r in range(1,4):
     for i in range(len(x)):
         levels[r-1].append([])
         for j in range(len(y)):
-            levels[r-1][i].append(f([i,j],r))
-
-print(np.array(levels).shape)
+            xy = [x[i], y[j]]
+            levels[r-1][i].append(f(np.transpose(xy),r))
 
 array1 = np.array(levels[0])
 array2 = np.array(levels[1])
 array3 = np.array(levels[2])
+'''
+array1 = array1[:, :, 0, 0]
+array2 = array2[:, :, 0, 0]
+array3 = array3[:, :, 0, 0]
+'''
 
-c1 = plt.contour(x, y, array1, [0])
-c2 = plt.contour(x, y, array2, [0])
-c2 = plt.contour(x, y, array3, [0])
+c1 = plt.contour(xx, yy, array1, [0], colors = 'r')
+c2 = plt.contour(xx, yy, array2, [0], colors = 'g')
+c3 = plt.contour(xx, yy, array3, [0], colors = 'b')
 plt.show()
 sys.exit()
 
+points = np.random.multivariate_normal(mean, cov, 10)
+bluePoints = []
+blackPoints = []
+for i in range(len(points)):
+    xy = points[i]
+    if (f(xy, 3)) > 0:
+        bluePoints.append(xy)
+    else:
+        blackPoints.append(xy)
 
-#x = np.random.multivariate_normal(mean, cov, 5).T
+print(bluePoints)
+print(blackPoints)
+
+#plt.plot(blackPoints)
+#plt.show()
 #res = f(x,1)
 #print res
-
-
-# You can use, np.meshgrid() and np.contour to make your life easier
