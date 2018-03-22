ba3a98dc6b6e3303c8fc46dcc46536ae3d5ed640
Lisa <sjlisa@student.chalmers.se>
2018-03-22 12:55:26 +0100

    Implemented r.

--- HW0/hw0_1.py
+++ HW0/hw0_1.py
@@ -13,23 +13,27 @@ def f(x,r):
     return (np.transpose(diff).dot(inverse).dot(diff))/2 - r
     #return ((np.array(map(operator.sub,x,mean)).T.tolist()*np.linalg.inv(cov)*map(operator.sub,x,mean))/2) - r
 
-x_ = np.linspace(-5, 5, num=200)
-y_ = np.linspace(-5, 5, num=200)
+x_ = np.linspace(-5, -4.5, num=200)
+y_ = np.linspace(-5, -4.5, num=200)
 x,y = np.meshgrid(x_, y_)
 levels = []
 
-
-for i in range(len(x)):
+for r in range(1,4):
     levels.append([])
-    for j in range(len(y)):
-        levels[i].append(f([i,j],1))
+    for i in range(len(x)):
+        levels[r-1].append([])
+        for j in range(len(y)):
+            levels[r-1][i].append(f([i,j],r))
 
-array = np.array(levels)
-print(array.shape)
+print(np.array(levels).shape)
 
+array1 = np.array(levels[0])
+array2 = np.array(levels[1])
+array3 = np.array(levels[2])
 
-c = plt.contour(x, y, array)
-plt.colorbar()
+c1 = plt.contour(x, y, array1, [0])
+c2 = plt.contour(x, y, array2, [0])
+c2 = plt.contour(x, y, array3, [0])
 plt.show()
 sys.exit()
 
