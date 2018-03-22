7124242cf0a5b3597498404af17a48e2de78ab02
Lisa <sjlisa@student.chalmers.se>
2018-03-22 11:57:35 +0100

    function update

--- HW0/hw0_1.py
+++ HW0/hw0_1.py
@@ -8,7 +8,10 @@ cov = [[0.1, -0.05], [-0.05, 0.2]]
 
 
 def f(x,r):
-    return ((np.array(map(operator.sub,x,mean)).T.tolist()*np.linalg.inv(cov)*map(operator.sub,x,mean))/2) - r
+    inverse = np.linalg.inv(cov)
+    diff = list(map(operator.sub, x, mean))
+    return (np.transpose(diff).dot(inverse).dot(diff))/2 - r
+    #return ((np.array(map(operator.sub,x,mean)).T.tolist()*np.linalg.inv(cov)*map(operator.sub,x,mean))/2) - r
 
 x_ = np.linspace(-5, 5, num=200)
 y_ = np.linspace(-5, 5, num=200)
@@ -22,7 +25,7 @@ for i in range(len(x)):
         levels[i].append(f([i,j],1))
 
 array = np.array(levels)
-array = array[:, :, 0, 0] # remove the two unwanted dimensions
+print(array.shape)
 
 
 c = plt.contour(x, y, array)
