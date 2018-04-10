8e027f8b6a09fef245f1c13d93897c1a7d734faa
Ellen Korsberg <korsberg.ellen@gmail.com>
2018-04-10 16:07:48 +0200

    Setup HW1

---
+++ HW1/hw1_1.py
@@ -0,0 +1,11 @@
+def sge(X):
+    """
+    SGE Mean and variance estimator for spherical Gaussian distribution
+
+    X : Data matrix of size n x p where each row represents a p-dimensional data point
+    e.g. X = [ 2 1 ; 3 7 ; 4 5 ] is a dataset having 3 samples having two co−ordinates each.
+    mu : Estimated mean o f the dataset [mu_1 mu_2 . . . mu_p]
+    sigma : Estimated standard deviation of the dataset ( number )
+    """
+
+    return mu,sigma
