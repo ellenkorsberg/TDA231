4433da6f0f5c33e0b99d1108bdc619dab5ace81d
Ellen Korsberg <korsberg.ellen@gmail.com>
2018-04-11 12:43:57 +0200

    Implemented sge and myplot1.

--- HW1/hw1_1.py
+++ HW1/hw1_1.py
@@ -1,11 +1,38 @@
-def sge(X):
-    """
-    SGE Mean and variance estimator for spherical Gaussian distribution
+import numpy as np
+import matplotlib.pyplot as plt
 
-    X : Data matrix of size n x p where each row represents a p-dimensional data point
-    e.g. X = [ 2 1 ; 3 7 ; 4 5 ] is a dataset having 3 samples having two co−ordinates each.
-    mu : Estimated mean o f the dataset [mu_1 mu_2 . . . mu_p]
-    sigma : Estimated standard deviation of the dataset ( number )
-    """
+def sge(X):
+    # Calculate mu
+    rows = X.shape[0]
+    mu = [0] * X.shape[1]
+    for i in range(rows):
+        mu += X[i][:]/rows
+
+    # Calculate sigma
+    sigma = 0
+    for i in range(rows):
+        sigma += np.dot(np.transpose(X[i][:]-mu), X[i][:]-mu)/rows
+    sigma = np.sqrt(sigma)
 
     return mu,sigma
+
+def myplot1(X):
+    est = sge(X)
+    fig, ax = plt.subplots(1)
+    ax.scatter(X[:,0],X[:,1])
+
+    theta = np.linspace(0, 2*np.pi, 100)
+    for i in range(1,4):
+        r = i*est[1]
+        x1 = r*np.cos(theta) + est[0][0]
+        x2 = r*np.sin(theta) + est[0][1]
+        ax.plot(x1, x2)
+        ax.set_aspect(1)
+
+    plt.show()
+
+
+# Load the data.
+X = np.loadtxt("dataset0.txt")
+X = X[:,:2]
+myplot1(X)
