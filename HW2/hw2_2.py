986e09c39be4937747795ecdc41660d110dc6659
Ellen Korsberg <korsberg.ellen@gmail.com>
2018-04-19 15:15:01 +0200

    Cleaned code.

--- HW2/hw2_2.py
+++ HW2/hw2_2.py
@@ -1,13 +1,25 @@
+import numpy as np
 from sklearn import datasets
 digits = datasets.load_digits()
 
 data = digits.data
-print(data.shape)
 target_names = digits.target_names
-print (target_names)
 import matplotlib.pyplot as plt
 y = digits.target
 #plt.matshow(digits.images[5])
 #plt.show()
 
-print(digits.return_X_y)
+print(digits.data)
+print(digits.target)
+
+fives = []
+eights = []
+
+for i in range(digits.target.shape[0]):
+    if digits.target[i] == 5:
+        fives.append(digits.data[:][i])
+    elif digits.target[i] == 8:
+        eights.append(digits.data[:][i])
+
+print(np.array(fives).shape)
+print(np.array(eights).shape)
