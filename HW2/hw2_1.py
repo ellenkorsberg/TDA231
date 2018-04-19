6a3f455cfee952a4dc919425ae4b056e5c4791df
Ellen Korsberg <korsberg.ellen@gmail.com>
2018-04-19 15:16:56 +0200

    Fix.

--- HW2/hw2_1.py
+++ HW2/hw2_1.py
@@ -18,12 +18,11 @@ def sge(X):
         mu += X[i][:]/rows
 
     # Calculate sigma
-    sigma = 0
+    sigmaSquare = 0
     for i in range(rows):
-        sigma += np.dot(np.transpose(X[i][:]-mu), X[i][:]-mu)/(rows*X.shape[1])
-    #sigma = np.sqrt(sigma)
+        sigmaSquare += np.dot(np.transpose(X[i][:]-mu), X[i][:]-mu)/(rows*X.shape[1])
 
-    return mu,sigma
+    return mu,sigmaSquare
 
 
 def sph_bayes(Xtest, trainingData): # other parameters needed.
@@ -41,7 +40,6 @@ def sph_bayes(Xtest, trainingData): # other parameters needed.
     return [postPos1, postNeg1, -1]
 
 
-
 def new_classifier(Xtest, mu1, mu2):
     b = mu1 + mu2
     b = [(1/2)*x for x in b]
@@ -51,78 +49,92 @@ def new_classifier(Xtest, mu1, mu2):
     return Ytest
 
 
+def scaleData(data):
+   scaledDataVar = []
+   for i in range(len(data)):
+       sample = data[i,:64]
+       maxGrey = np.max(sample)
+       scaledSample = sample/maxGrey
+       scaledSample = scaledSample.reshape((8,8))
+       variance = np.zeros(16)
 
-#
-# # 5-fold cross validation error for Bayes classifier
-# X = data
-# kf = KFold(n_splits=5, shuffle=True)
-# error = 0
-# for train_index, test_index in kf.split(X):
-#    X_train, X_test = X[train_index], X[test_index]
-#    for i in range(X_test.shape[0]):
-#        pred = sph_bayes(X_test[:,:3][i], X_train)
-#        if pred[2] != X_test[:][i][3]:
-#            error += 1
-#
-# print("Bayes error: ", error/5)
-#
-# # 5-fold cross validation error for other classifier
-# error = 0
-# for train_index, test_index in kf.split(X):
-#    X_train, X_test = X[train_index], X[test_index]
-#    for i in range(X_test.shape[0]):
-#        mu1 = sge(X_train[X_train[:,3] == 1][:,:3])[0]
-#        mu2 = sge(X_train[X_train[:,3] == -1][:,:3])[0]
-#        pred = new_classifier(X_test[:,:3][i], mu1, mu2)
-#        if pred != X_test[:][i][3]:
-#            error += 1
-#
-# print("Other error: ", error/5)
+       for j in range(len(scaledSample)):
+           variance[j] = np.var(scaledSample[j][:])
+           variance[j+8] = np.var(scaledSample[:][j])
+
+       scaledDataVar.append(np.append(variance, data[i][64]))
+
+   return np.array(scaledDataVar)
 
 
+def crossValidationBayes(data, labelIndex):
+    X = data
+    kf = KFold(n_splits=5, shuffle=True)
+    error = 0
+    for train_index, test_index in kf.split(X):
+       X_train, X_test = X[train_index], X[test_index]
+       for i in range(X_test.shape[0]):
+           pred = sph_bayes(X_test[:,:3][i], X_train)
+           if pred[2] != X_test[:][i][labelIndex]:
+               error += 1
 
-import numpy as np
-from sklearn import datasets
-digits = datasets.load_digits()
-
-data = digits.data
-target_names = digits.target_names
-import matplotlib.pyplot as plt
-y = digits.target
-
-
-fives = []
-eights = []
-
-for i in range(digits.target.shape[0]):
-    if digits.target[i] == 5:
-        fives.append(digits.data[:][i])
-    elif digits.target[i] == 8:
-        eights.append(digits.data[:][i])
-
-fives = np.array(fives)
-eights = np.array(eights)
+    return error/5
 
 
-mu1 = sge(fives)[0]
-mu2 = sge(eights)[0]
-pred = new_classifier(digits.images[5].flatten(), mu1, mu2)
-print(pred)
-plt.matshow(digits.images[5])
-#plt.show()
+def crossValidationNewClassifier(data, labelIndex):
+    X = data
+    kf = KFold(n_splits=5, shuffle=True)
+    error = 0
+    for train_index, test_index in kf.split(X):
+       X_train, X_test = X[train_index], X[test_index]
+       for i in range(X_test.shape[0]):
+           mu1 = sge(X_train[X_train[:,labelIndex] == 1][:,:labelIndex])[0]
+           mu2 = sge(X_train[X_train[:,labelIndex] == -1][:,:labelIndex])[0]
+           pred = new_classifier(X_test[:,:labelIndex][i], mu1, mu2)
+           if pred != X_test[:][i][labelIndex]:
+               error += 1
 
-scaledDataVar = []
-for i in range(len(data)):
-    sample = data[i][:]
-    maxGrey = np.max(sample)
-    scaledSample = sample/maxGrey
-    scaledSample = scaledSample.reshape((8,8))
-    variance = np.zeros(16)
-    # rows:
-    for j in range(len(scaledSample)): #len(scaledSample)
-        variance[j] = np.var(scaledSample[j][:])
-        variance[j+8] = np.var(scaledSample[:][j])
+    return error/5
 
-    scaledDataVar.append(variance)
 
-print(np.array(scaledDataVar).shape)
+def firstPracticalProblem():
+    data = genfromtxt('dataset2.txt', delimiter=',')
+    print("Bayes classifier error: ", crossValidationBayes(data, 3))
+    print("New (sign) classifier error: ", crossValidationNewClassifier(data, 3))
+
+
+def getFiveAndEightClasses(digits):
+    fives = []
+    eights = []
+
+    for i in range(digits.target.shape[0]):
+        if digits.target[i] == 5:
+            fives.append(np.append(digits.data[:][i], 1))
+        elif digits.target[i] == 8:
+            eights.append(np.append(digits.data[:][i], -1))
+
+    fives = np.array(fives)
+    eights = np.array(eights)
+
+    return np.concatenate((fives,eights))
+
+
+def secondPracticalProblem():
+    digits = datasets.load_digits()
+    data = getFiveAndEightClasses(digits)
+    print("New (sign) classifier error with original feature vector: ", crossValidationNewClassifier(data, 64))
+    print("New (sign) classifier error with another feature vector: ", crossValidationNewClassifier(scaleData(data), 16))
+
+    # IS THIS NEEDED?
+
+    # mu1 = sge(data[data[:,64] == 1][:,:64])[0] # mean of fives class
+    # mu2 = sge(data[data[:,64] == -1][:,:64])[0] # mean of eights class
+    # pred = new_classifier(digits.images[5].flatten(), mu1, mu2)
+    # print(pred)
+    # plt.matshow(digits.images[5])
+    # plt.show()
+
+
+
+#firstPracticalProblem()
+secondPracticalProblem()
