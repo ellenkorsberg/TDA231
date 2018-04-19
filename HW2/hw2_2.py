c7012a6ffbcbd6edef91f8eb3863efea2ac48c77
Ellen Korsberg <korsberg.ellen@gmail.com>
2018-04-19 12:42:47 +0200

    Setup practical problem 2.

---
+++ HW2/hw2_2.py
@@ -0,0 +1,13 @@
+from sklearn import datasets
+digits = datasets.load_digits()
+
+data = digits.data
+print(data.shape)
+target_names = digits.target_names
+print (target_names)
+import matplotlib.pyplot as plt
+y = digits.target
+#plt.matshow(digits.images[5])
+#plt.show()
+
+print(digits.return_X_y)
