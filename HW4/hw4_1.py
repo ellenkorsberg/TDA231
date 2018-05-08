e34074bb1ab658627264b59a4c4310f1dcc5b050
Ellen Korsberg <korsberg.ellen@gmail.com>
2018-05-08 15:58:10 +0200

    Nice plot.

--- HW4/hw4_1.py
+++ HW4/hw4_1.py
@@ -13,13 +13,16 @@ negativeIndices = np.where(Y == -1)[0]
 clf = SVC(kernel='linear')
 clf.fit(X, Y)
 
+print("The bias is %s" % clf.intercept_[0])
+
 # Plotting the data and the decision boundary
 w = clf.coef_[0]
 a = -w[0] / w[1]
 xx = np.linspace(np.min(X)-1, np.max(X)+1)
 yy = a * xx - (clf.intercept_[0]) / w[1]
-plt.plot(X[positiveIndices,0], X[positiveIndices,1], 'r.')
-plt.plot(X[negativeIndices,0], X[negativeIndices,1], 'b.')
+plt.plot(X[positiveIndices,0], X[positiveIndices,1], 'ro')
+plt.plot(X[negativeIndices,0], X[negativeIndices,1], 'bo')
+plt.plot(X[clf.support_,0], X[clf.support_, 1], 'w.')
 plt.plot(xx, yy, 'k-')
 
 
@@ -30,4 +33,3 @@ for i in range(1,200):
         plt.plot(X[i,0], X[i,1], 'go')
 
 plt.show()
-print(misclassified)
