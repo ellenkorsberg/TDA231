1d78f6ec9aa72f54a5c89791e7b4288f4b10b5be
Lisa <sjlisa@student.chalmers.se>
2018-05-08 14:49:06 +0200

    Plotting the data.

--- HW4/hw4_1.py
+++ HW4/hw4_1.py
@@ -1,4 +1,14 @@
 import numpy as np
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+
 data = np.loadtxt('d1.txt')
 X = data[:,:2]
 Y = data[:,-1]
+
+positiveIndices = np.where(Y == 1)[0]
+negativeIndices = np.where(Y == -1)[0]
+
+plt.plot(X[positiveIndices,0], X[positiveIndices,1], 'r.')
+plt.plot(X[negativeIndices,0], X[negativeIndices,1], 'b.')
+plt.show()
